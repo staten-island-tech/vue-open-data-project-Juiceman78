ebb059392469b279c7c6e8d10022f1c3a418a844
Juiceman78 <vincentz78@nycstudents.net>
2026-03-18 14:00:23 -0400

    Triple windmill table header method

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,32 @@
-<script setup></script>
-
 <template>
-  <main>
-    <h1>Police Data</h1>
-  </main>
+  <div>
+    <h1>NYC Criminal Data</h1>
+
+    <div v-for="item in data" :key="item.id">
+      <pre>{{ item }}</pre>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+
+const data = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+    const result = await response.json()
+    data.value = result
+  } catch (error) {
+    console.log('Error')
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
